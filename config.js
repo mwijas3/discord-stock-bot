f1158bf4cbe474b1bcdea2d445a6be435fa7cb09
mwijas3 <64617273+mwijas3@users.noreply.github.com>
2020-06-11 11:58:29 -0400

    Update and rename example-config.js to config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+	BOT_TOKEN: 'https://discordapp.com/api/webhooks/720652089042272346/spfOggx2fZBHwQtFEpwcDRuEN-K-tKbuamrZjblbhKKv-3SZYDDLjI1t7cbdwEiJ43VE',
+	MORNING_BELL_CHANNEL_ID: '#coming-soon'
+};
+
+module.exports = config;
